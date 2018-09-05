73fcd1f1d4d5f97b5595f4570983db69b772db27
Dante <dante.vacirca1@gmail.com>
2018-09-05 19:39:31 +1000

    master branch

---
+++ restaurantApp/app/src/main/java/com/appcrew/restaurantApp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.appcrew.restaurantApp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        );
+    }
+}
